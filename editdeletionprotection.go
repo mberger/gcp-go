5ce45a324895642b7261e5d92896ced9b3ff89b3
Michael Berger <mberger@internetinnovation.com>
2019-12-18 09:33:11 -0500

    deletion protreciton script initial commit

---
+++ editdeletionprotection.go
@@ -0,0 +1,55 @@
+package main
+
+// BEFORE RUNNING:
+// ---------------
+// 1. If not already done, enable the Compute Engine API
+//    and check the quota for your project at
+//    https://console.developers.google.com/apis/api/compute
+// 2. This sample uses Application Default Credentials for authentication.
+//    If not already done, install the gcloud CLI from
+//    https://cloud.google.com/sdk/ and run
+//    `gcloud beta auth application-default login`.
+//    For more information, see
+//    https://developers.google.com/identity/protocols/application-default-credentials
+// 3. Install and update the Go dependencies by running `go get -u` in the
+//    project directory.
+
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/compute/v1"
+)
+
+func main() {
+	ctx := context.Background()
+
+	c, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	computeService, err := compute.New(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Project ID for this request.
+	project := "my-project" // TODO: Update placeholder value.
+
+	// The name of the zone for this request.
+	zone := "my-zone" // TODO: Update placeholder value.
+
+	// Name of the resource for this request.
+	resource := "my-resource" // TODO: Update placeholder value.
+
+	resp, err := computeService.Instances.SetDeletionProtection(project, zone, resource).Context(ctx).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO: Change code below to process the `resp` object:
+	fmt.Printf("%#v\n", resp)
+}
